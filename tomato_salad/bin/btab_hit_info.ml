4a518b5643804a92a8991551f5bf44d71bdd5026
Ryan M. Moore <rmm1047@gmail.com>
2022-03-04 01:33:34 -0500

    Add btab_hit_info script

---
+++ tomato_salad/bin/btab_hit_info.ml
@@ -0,0 +1,115 @@
+open! Base
+module U = Tomato_salad.Utils
+
+module Cli = struct
+  let version = "0.1.0"
+
+  open Cmdliner
+
+  type opts = { hit_info : string; btab : string }
+
+  let make_opts hit_info btab = { hit_info; btab }
+
+  let info_term =
+    let doc =
+      "Path to target/hit info file.  Should be [id TAB info].  Repeated IDs \
+       in the info file trigger exceptions."
+    in
+    Arg.(
+      required & pos 0 (some non_dir_file) None & info [] ~docv:"HIT_INFO" ~doc)
+
+  let btab_term =
+    let doc = "Path to btab file." in
+    Arg.(required & pos 1 (some non_dir_file) None & info [] ~docv:"BTAB" ~doc)
+
+  let info =
+    let doc = "Get info from a btab file" in
+    let man =
+      [
+        `S Manpage.s_description;
+        `P
+          "Get hit info from a btab file, but also print out some info about \
+           the hits.";
+        `S Manpage.s_examples;
+        `Pre "  \\$ btab_hit_info hit_info.tsv btab.tsv > query_hit_info.txt";
+      ]
+    in
+    Cmd.info "pick_lines" ~doc ~man ~version
+
+  let term = Term.(const make_opts $ info_term $ btab_term)
+
+  let parse () =
+    match Cmd.eval_value @@ Cmd.v info term with
+    | Ok (`Ok opts) -> `Run opts
+    | Ok `Help | Ok `Version -> `Exit 0
+    | Error _ -> `Exit 1
+end
+
+module Scores = struct
+  type t = float list Map.M(String).t
+
+  let init target target_bits =
+    Map.of_alist_exn (module String) [ (target, [ target_bits ]) ]
+
+  let update t target target_bits =
+    Map.update t target ~f:(function
+      | None -> [ target_bits ]
+      | Some scores -> target_bits :: scores)
+end
+
+type query_info = { scores : Scores.t; total_bits : float; total_hits : int }
+
+module Hit_info = struct
+  type t = string Map.M(String).t
+
+  let read_file fname : t =
+    U.with_file_fold_lines fname
+      ~init:(Map.empty (module String))
+      ~f:(fun ids line ->
+        match String.split ~on:'\t' line with
+        | [ id; info ] -> Map.add_exn ids ~key:id ~data:info
+        | _ -> U.abort [%string "ERROR: Bad info line: '%{line}'"])
+end
+
+let process_btab fname target_info =
+  let open Bio_io.Mmseqs in
+  let queries =
+    In_channel.with_file_fold_records_exn fname
+      ~init:(Map.empty (module String))
+      ~f:(fun queries r ->
+        (* Use the info instead of the actual target if it is present. *)
+        let target =
+          Option.value ~default:r.target @@ Map.find target_info r.target
+        in
+        Map.update queries r.query ~f:(function
+          | None ->
+              let scores = Scores.init target r.bits in
+              { scores; total_bits = r.bits; total_hits = 1 }
+          | Some { scores; total_bits; total_hits } ->
+              let scores = Scores.update scores target r.bits in
+              {
+                scores;
+                total_bits = total_bits +. r.bits;
+                total_hits = total_hits + 1;
+              }))
+  in
+  Map.iteri queries
+    ~f:(fun ~key:query ~data:{ scores; total_bits; total_hits } ->
+      Map.iteri scores ~f:(fun ~key:target ~data:scores ->
+          let target_hits, target_bits =
+            List.fold scores ~init:(0, 0.) ~f:(fun (num_hits, bits) this ->
+                (num_hits + 1, bits +. this))
+          in
+          let target_bit_prop = target_bits /. total_bits in
+          let target_hit_prop =
+            Float.(of_int target_hits / of_int total_hits)
+          in
+          Stdio.printf "%s\t%s\t%.0f\t%.3f\t%d\t%.3f\n" query target target_bits
+            target_bit_prop target_hits target_hit_prop))
+
+let () =
+  let opts : Cli.opts =
+    match Cli.parse () with `Run file -> file | `Exit code -> Caml.exit code
+  in
+  let hit_info = Hit_info.read_file opts.hit_info in
+  process_btab opts.btab hit_info
---
+++ tomato_salad/test/btab_hit_info.t/run.t
@@ -0,0 +1,8 @@
+Hi
+
+  $ btab_hit_info info.tsv btab.tsv
+  Q1	T111	35	0.368	1	0.250
+  Q1	cool seq	60	0.632	3	0.750
+  Q2	cool seq	40	0.444	1	0.500
+  Q2	really snazzy	50	0.556	1	0.500
+  Q3	i like this one too	60	1.000	1	1.000
